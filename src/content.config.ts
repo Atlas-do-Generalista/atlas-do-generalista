9238988721469921533625cc976aa2828b966dda
jpedrolelisf <jpedrolelis@gmail.com>
2026-08-26 17:58:37 -0300

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,16 @@
-import { defineCollection } from 'astro:content';
-import { docsLoader } from '@astrojs/starlight/loaders';
+import { defineCollection, z } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+	docs: defineCollection({
+		schema: docsSchema({
+			extend: z.object({
+				autor: z.string().optional(),
+				orcid: z.string().optional(),
+				revisor: z.string().optional(),
+				date: z.date().optional(),
+				doi: z.string().optional(),
+			}),
+		}),
+	}),
 };
