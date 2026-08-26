1d06ef44e66233e360b73dbee9a0b6f0a1fd79fc
jpedrolelisf <jpedrolelis@gmail.com>
2026-08-26 18:27:22 -0300

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,18 @@
 import { defineCollection, z } from 'astro:content';
+import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
-	docs: defineCollection({
-		schema: docsSchema({
-			extend: z.object({
-				autor: z.string().optional(),
-				orcid: z.string().optional(),
-				revisor: z.string().optional(),
-				date: z.date().optional(),
-				doi: z.string().optional(),
-			}),
-		}),
-	}),
+  docs: defineCollection({
+    loader: docsLoader(),
+    schema: docsSchema({
+      extend: z.object({
+        autor: z.string().optional(),
+        orcid: z.string().optional(),
+        revisor: z.string().optional(),
+        date: z.coerce.date().optional(),
+        doi: z.string().optional(),
+      }),
+    }),
+  }),
 };
